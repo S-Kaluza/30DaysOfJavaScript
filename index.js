fb9b07e31e6bac653d8a55fee8990e44da0fc6bc
szymo <szymon.i.kaluza@gmail.com>
2022-08-23 14:11:46 +0200

    second day of challange

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const hello = () => {
-    document.write("hello world");
-    console.log("branch day 01");
+function rot(){
+    const date = new Date()
+    console.log(date)
+    let rad = (date.getSeconds() + 15) * 6 + 1;
+    let rad1 = (date.getMinutes() + 15) * 6;
+    const trans = setInterval(() => {
+        if(rad === 360) rad = 0;
+        document.getElementById('line').style.transform = `rotate(${rad}deg)`;
+        document.getElementById('line2').style.transform = `rotate(${rad1}deg)`;
+        rad += 6;
+        rad1 += 0.000277777778;
+    }, 1000);
 }
