b074e71d5101a275b2f6ee4c77225e678d3ceb2b
szymo <szymon.i.kaluza@gmail.com>
2022-08-24 20:36:33 +0200

    third day

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
-function rot(){
-    const date = new Date()
-    console.log(date)
-    let rad = (date.getSeconds() + 15) * 6 + 1;
-    let rad1 = (date.getMinutes() + 15) * 6;
-    const trans = setInterval(() => {
-        if(rad === 360) rad = 0;
-        document.getElementById('line').style.transform = `rotate(${rad}deg)`;
-        document.getElementById('line2').style.transform = `rotate(${rad1}deg)`;
-        rad += 6;
-        rad1 += 0.000277777778;
-    }, 1000);
+let number = 0;
+let op = 0;
+let times;
+window.onload = () => {
+    number += 1;
+    document.getElementById(`lang-${number}`).style.display = 'inline-block';
+    fadeUp();
+    
+}
+
+function fadeUp() {
+    times = setInterval(() => {
+        document.getElementById(`lang-${number}`).style.display = 'inline-block';
+        if(document.getElementById(`lang-${number}`).style.opacity >= 1){
+            clearInterval(times);
+        }
+        document.getElementById(`lang-${number}`).style.opacity = op;
+        op += 0.005;
+        console.log(op);
+    }, 10);
+}
+
+function fadeDown() {
+    times = setInterval(() => {
+        if(document.getElementById(`lang-${number}`).style.opacity === 0){
+            clearInterval(times);
+        }
+        document.getElementById(`lang-${number}`).style.opacity = op;
+        op -= 0.005;
+    }, 10);
+}
+
+function nextLang(){
+    clearInterval(times);
+    op = 1;
+    times = setInterval(() => {
+        if(document.getElementById(`lang-${number}`).style.opacity <= 0){
+            document.getElementById(`lang-${number}`).style.display = 'none';
+            if(number < 4) number += 1;
+            else number = 1;
+            document.getElementById(`lang-${number}`).style.display = 'inline-block';
+            op = 0;
+            clearInterval(times);
+            times = setInterval(() => {
+                if(document.getElementById(`lang-${number}`).style.opacity >= 1){
+                    clearInterval(times);
+                }
+                document.getElementById(`lang-${number}`).style.opacity = op;
+                op += 0.005;
+                console.log(number);
+            }, 10)
+        }
+        document.getElementById(`lang-${number}`).style.opacity = op;
+        op -= 0.005;
+        console.log(op);
+    }, 10)
+}
+
+function prevLang(){
+    clearInterval(times);
+    op = 1;
+    times = setInterval(() => {
+        if(document.getElementById(`lang-${number}`).style.opacity <= 0){
+            document.getElementById(`lang-${number}`).style.display = 'none';
+            if(number > 1) number -= 1;
+            else number = 4;
+            document.getElementById(`lang-${number}`).style.display = 'inline-block';
+            op = 0;
+            clearInterval(times);
+            times = setInterval(() => {
+                if(document.getElementById(`lang-${number}`).style.opacity >= 1){
+                    clearInterval(times);
+                }
+                document.getElementById(`lang-${number}`).style.opacity = op;
+                op += 0.005;
+                console.log(number);
+            }, 10)
+        }
+        document.getElementById(`lang-${number}`).style.opacity = op;
+        op -= 0.005;
+        console.log(op);
+    }, 10)
 }
